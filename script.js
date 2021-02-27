29c9152e599e513b8f7e5c98246129d455de427e
Vinicius Lopes Simoes <vini.lopes.simoes@gmail.com>
2021-02-27 18:38:50 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+function countdown() {
+    const now = new Date();
+
+    const nextYear = now.getFullYear() + 1;
+    const newYearsDate = new Date(nextYear, 0, 1, 0, 0, 0, 0);
+
+    const totalSeconds = (newYearsDate - now) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+setInterval(countdown, 1000);
